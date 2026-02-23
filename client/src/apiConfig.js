f457111b0547f8f8559967307ca36a803b6d490c
khalil-mejri-1 <mejrik1888@gmail.com>
2026-02-24 00:55:28 +0100

    final update-9 2/24/2026

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,7 +1,7 @@
 // 🚀 Configuration Centralisée des URLs Backend
 // Changez cette variable pour basculer rapidement entre le serveur local et Vercel
 
-// const BASE_URL = 'https://2c-patron-ddml.vercel.app'; // Pour le développement local
-const BASE_URL = 'https://2c-patron-ddml.vercel.app'; // P  our la production
+// const BASE_URL = 'https://2c-patron-2hcu.vercel.app'; // Pour le développement local
+const BASE_URL = 'https://2c-patron-2hcu.vercel.app'; // P  our la production
 
 export default BASE_URL;
