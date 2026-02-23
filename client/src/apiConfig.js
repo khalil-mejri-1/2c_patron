0bd21333a998d48d97aa31e5e8e10483b67d066b
khalil-mejri-1 <mejrik1888@gmail.com>
2026-02-24 00:51:30 +0100

    final update-9 2/24/2026

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -2,6 +2,6 @@
 // Changez cette variable pour basculer rapidement entre le serveur local et Vercel
 
 // const BASE_URL = 'https://2c-patron-ddml.vercel.app'; // Pour le développement local
-const BASE_URL = 'http://localhost:3000'; // P  our la production
+const BASE_URL = 'https://2c-patron-ddml.vercel.app'; // P  our la production
 
 export default BASE_URL;
